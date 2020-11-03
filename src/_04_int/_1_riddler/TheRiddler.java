2960d121eac554bcbc3a397060be233c91d46184
root <root@8ddd746a0f95>
2020-11-03 02:26:48 +0000

    day 17 of coding

--- src/_04_int/_1_riddler/TheRiddler.java
+++ src/_04_int/_1_riddler/TheRiddler.java
@@ -9,11 +9,11 @@ public class TheRiddler {
 	public static void main(String[] args) {
 
 		// 1. Make a variable to hold the score
-
+		int score = 0;
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
-
+		String input = JOptionPane.showInputDialog(null, "I am not alive, but I grow;I don't have lungs, but I need air; I don't have a mouth, but water kills me. What am I?");
 		// 4. If they got the answer right, pop up "correct!" and increase the score by one
-
+		
 		// 5. Otherwise, say "wrong" and tell them the answer
 
 		// 6. Add some more riddles
