0c545bb5c062bceddc971707f94acdb7d87ced83
root <root@031e3c7a3ba4>
2020-11-10 02:26:07 +0000

    day 18 of coding

--- src/_04_int/_1_riddler/TheRiddler.java
+++ src/_04_int/_1_riddler/TheRiddler.java
@@ -1,8 +1,9 @@
 package _04_int._1_riddler;
+
+import javax.swing.JOptionPane;
+
 //    Copyright (c) The League of Amazing Programmers 2013-2019
 //    Level 0
-
-
 
 public class TheRiddler {
 
@@ -11,15 +12,22 @@ public class TheRiddler {
 		// 1. Make a variable to hold the score
 		int score = 0;
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
-		String input = JOptionPane.showInputDialog(null, "I am not alive, but I grow;I don't have lungs, but I need air; I don't have a mouth, but water kills me. What am I?");
-		// 4. If they got the answer right, pop up "correct!" and increase the score by one
-		
-		// 5. Otherwise, say "wrong" and tell them the answer
+		String input = JOptionPane.showInputDialog(null,
+				"I am not alive, but I grow;I don't have lungs, but I need air; I don't have a mouth, but water kills me. What am I?");
+		// 4. If they got the answer right, pop up "correct!" and increase the score by
+		// one
+		if (input.equals("fire")) {
+			JOptionPane.showMessageDialog(null, "correct!");
 
+			score++;
+		}
+		// 5. Otherwise, say "wrong" and tell them the answer
+		else {
+			JOptionPane.showMessageDialog(null, "wrong.");
+		}
 		// 6. Add some more riddles
 
 		// 2. Make a pop up to show the score.
-		
+
 	}
 }
-
