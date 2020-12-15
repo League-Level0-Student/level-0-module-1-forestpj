cf8cd2b93602c12e636ae5e45cefe5a9a966945b
root <root@09359acc34a1>
2020-12-15 02:28:41 +0000

    Day 25 of coding

--- src/_05_for_loops/_1_my_ages/MyAges.java
+++ src/_05_for_loops/_1_my_ages/MyAges.java
@@ -1,41 +1,46 @@
 package _05_for_loops._1_my_ages;
 
+import org.omg.Messaging.SyncScopeHelper;
+
 public class MyAges {
 	public static void main(String[] args) {
-		
-		/** This program will use loops to print all the ages you have been alive, 
-		 * from zero up to your current age. **/
 
-		// Make an int variable called age, to track how many times the loop has repeated
-		//        Start its value at zero.
-		
-		// Change the value of yourCurrentAge to match your age
-		int yourCurrentAge = 0;
-				
-		
-		//  WHILE LOOP. Start a while loop to repeat while age <= yourCurrentAge
-				
-			//  Print the age variable
-		
-			//  Add one to the age variable
-		
-		//  End the while loop here
-
-		//  Run the program. Did it print all your ages from 0 to your current age?
-		
-		/*** Now you are going to do the same thing, but this time use a for loop. 
-		 *   Notice how most of the information is the same, it is just written 
-		 *   in a different way.
+		/**
+		 * This program will use loops to print all the ages you have been alive, from
+		 * zero up to your current age.
 		 **/
-		
-		//  FOR LOOP. Start a for loop to repeat while age <= your age, like this:
-		//  for ( age=0; age <= yourAge; age +=1 ) {
-		
-			//  Print the age variable
-		
-		//  End the for loop here
-		
-		//  Run the program again. 		
-		//  Check that both loops printed the same numbers to the console
+
+		// Make an int variable called age, to track how many times the loop has
+		// repeated
+		// Start its value at zero.
+		int age = 0;
+		// Change the value of yourCurrentAge to match your age
+		int yourCurrentAge = 12;
+
+		// WHILE LOOP. Start a while loop to repeat while age <= yourCurrentAge
+		while (age <= yourCurrentAge) {
+			// Print the age variable
+			System.out.println("while " + age);
+			// Add one to the age variable
+			age++;
+			// End the while loop here
+		}
+		// Run the program. Did it print all your ages from 0 to your current age?
+
+		/***
+		 * Now you are going to do the same thing, but this time use a for loop. Notice
+		 * how most of the information is the same, it is just written in a different
+		 * way.
+		 **/
+
+		// FOR LOOP. Start a for loop to repeat while age <= your age, like this:
+		// for ( age=0; age <= yourAge; age +=1 ) {
+		for (age = 0; age <= yourCurrentAge; age +=1) {
+			// Print the age variable
+			System.out.println("for " + age);
+			// End the for loop here
+		}
 	}
+	// Run the program again.
+	// Check that both loops printed the same numbers to the console
 }
---
+++ src/_05_for_loops/_2_badgers/Badgers.java
@@ -0,0 +1,17 @@
+package _05_for_loops._2_badgers;
+
+import javax.swing.JOptionPane;
+
+public class Badgers {
+	public static void main(String[] args) {
+		
+		for (int i=10;i<=10;i++);
+		
+		JOptionPane.showMessageDialog(null, "Badger");
+		
+		
+		
+		
+		
+	}
+}
