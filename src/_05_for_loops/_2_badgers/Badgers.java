eff03ddb44ae4e3adb34ffef7d63bcecb4f12c94
root <root@c44acaa3fd22>
2020-12-22 02:26:42 +0000

    day 26 of coding

--- src/_05_for_loops/_2_badgers/Badgers.java
+++ src/_05_for_loops/_2_badgers/Badgers.java
@@ -5,13 +5,21 @@ import javax.swing.JOptionPane;
 public class Badgers {
 	public static void main(String[] args) {
 		
-		for (int i=10;i<=10;i++);
-		
+		for (int i=1;i<=12;i++) {
 		JOptionPane.showMessageDialog(null, "Badger");
-		
-		
-		
-		
+		}
+		for (int i=1;i<=2;i++) {
+		JOptionPane.showMessageDialog(null, "Mushroom");
+		}
+		for (int i=1;i<=12;i++) {
+		JOptionPane.showMessageDialog(null, "Badger");
+		}
+		for (int i=1;i<=2;i++) {
+		JOptionPane.showMessageDialog(null, "Mushroom");
+		}
+		for (int i=1;i<=1;i++) {
+		JOptionPane.showMessageDialog(null, "A Snake!!!");
+		}
 		
 	}
 }
---
+++ src/_05_for_loops/_4_banana_split/banana_split/banana_split.pde
@@ -0,0 +1,7 @@
+int x = 400; 
+int y = ; 
+int movement = 5;
+for (int i=0;i<3;i++) {
+text("Ice cream", 22, 10);
+x = x - movement;
+}
